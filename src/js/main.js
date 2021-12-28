37f9887bd78ba88be08346abc2092ffa7602933f
Babsrm <babsruizm@gmail.com>
2021-12-28 20:21:42 +0100

    add JS reset button

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,56 @@
-'use strict';
+/* eslint-disable no-undef */
+"use strict";
 
-console.log('>> Ready :)');
+//Nos traemos los elementos del HTML
+const userInput = document.querySelector(".js-userInput");
+const searchBtn = document.querySelector(".js-searchBtn");
+const resetBtn = document.querySelector(".js-resetBtn");
+
+//data objeto
+const data = {
+  input: "",
+};
+
+// funcion input del usuario
+function handlerUserInput() {
+  if (userInput.value === "") {
+    data.input = "";
+  } else {
+    data.input = userInput.value;
+  }
+}
+
+userInput.addEventListener("keyup", handlerUserInput);
+
+//funcion botón reset
+function handleResetBtn() {
+  userInput.value = "";
+  data.name = "";
+}
+
+resetBtn.addEventListener("click", handleResetBtn);
+
+//funcion botón search
+const apiResults = document.querySelector(".js-resultsData");
+
+function handleSearchBtn(event) {
+  event.preventDefault();
+  fetch("//api.jikan.moe/v3/search/anime?q=naruto", {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: { "content-Type": "application/json" },
+  })
+    .then((response) => response.json())
+    .then((dataApi) => {
+      if (dataApi.request_cached === true) {
+       //si me da resultado, quiero añadirlo en el html (seria objeto?)
+       //tb debería de añadirlo al local para que se guarde
+       apiResults.innerHTML = dataApi.results;
+      } else {
+        //si no me da resultado, pues... ¿quiero que aparezca un error de que no hay nada?
+      }
+    });
+}
+
+searchBtn.addEventListener("click", handleSearchBtn); //que funcione con click
+searchBtn.addEventListener("enter", handleSearchBtn); //que funcione con enter
