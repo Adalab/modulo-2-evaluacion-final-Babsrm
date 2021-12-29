a6d711ff908738a4c4098177d53524dbc92e10fa
Babsrm <babsruizm@gmail.com>
2021-12-29 13:10:31 +0100

    set local storage and inner HTML

--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,19 @@
 const userInput = document.querySelector(".js-userInput");
 const searchBtn = document.querySelector(".js-searchBtn");
 const resetBtn = document.querySelector(".js-resetBtn");
+const apiResults = document.querySelector(".js-resultsData");
+const favListElem = document.querySelector(".js-favoritesList");
+const deletefavElem = document.querySelector(".js-resetBtnfav");
+const formElement = document.querySelector(".js-form");
+
+
+//constantes y variables que usaremos
+const apiUrl = 'https://api.jikan.moe/v3/search/anime';
+const imgIfNone = 'https://via.placeholder.com/227x320/e5e5e5/666/?text=TV';
+
+let shows = [];
+let favorites =[];
+
 
 //data objeto
 const data = {
@@ -31,50 +44,118 @@ function handleResetBtn() {
 resetBtn.addEventListener("click", handleResetBtn);
 
 //funcion botón search. Cogemos la data de la API
-//const apiResults = document.querySelector(".js-resultsData");
-let apiResults = [];
 
-function handleSearchBtn(event) {
-  event.preventDefault();
-  fetch("https://api.jikan.moe/v3/search/anime?q=`${userInput.value}`")
+function getShowFromApi(txtInput) {
+  txtInput.preventDefault();
+  fetch(apiUrl + txtInput)
     .then((response) => response.json())
     .then((dataApi) => {
-      apiResults = dataApi.results;
+      apiResults = [];
       paintShows;
     });
+    getShowsFromApiResults();
 };
 
 //pintar resultados de la api
-const resultsData = document.querySelector(".js-resultsData");
+
 
 const getShowsFromApiResults = (shows) => {
+  const showListElem = document.querySelector(".js-seriesList");
+
   let htmlCode = "";
-  htmlCode += `<article class= "showsResult"`;
-  htmlCode += `<img src="${results.image_url}" class="show__img" alt= "Show: ${results.title}">`;
+
+  for (const show of shows) {
+    let isShowClass;
+    let showInFavorites = favorites.findIndex(
+      (favorite) => favorite.id === show.id
+    );
+
+    if (showInFavorites === -1) {
+      isShowClass = "serie";
+    } else {
+      isShowClass = "selected";
+    }
+  htmlCode += `<li id ="${show.id}" class="js-serie li__show ${isShowClass} >`;
   htmlCode += `<h5 class= "show__title">${results.title}</h5>`;
   htmlCode += `<button class= "show__favbtn">Añadir a favoritos</button>`;
-  htmlCode += `</article>`;
+  if (show.img_url === null) {
+    imgIfNone;
+  }
+  else {
+    htmlCode += `<img src="${results.image_url}" class="show__img" alt= "Show: ${results.title}">`;
+  }
+  htmlCode += `</li>`;
+  
   return htmlCode;
 };
-const paintShows = () => {
-  let showsCode = "";
-  for (const show of shows) {
-    showsCode += getShowsFromApiResults(shows);
-  }
-  resultsData.innerHTML = showsCode;
-};
 
-// {apiResults = dataApi.results;
-//if (dataApi.request_cached === true) {
-//si me da resultado, quiero añadirlo en el html (seria objeto?)
-//tb debería de añadirlo al local para que se guarde
+apiResults.innerHTML = htmlCode;
 
-//console.log(dataApi);
-//} else {
-//si no me da resultado, pues... ¿quiero que aparezca un error de que no hay nada?
-// }
-// });
+//buscar la entrada de datos
+function handleSearchBtn() {
+  let inputValue = userInput.value;
+  getShowsFromApiResults(inputValue);}
 
 searchBtn.addEventListener("click", handleSearchBtn); //que funcione con click
 // searchBtn.addEventListener("keypress", handleSearchBtn){
 //    if (e.key === "Enter"){handleSearchBtn}}; //que funcione con enter
+
+// enviar formulario
+function handleForm(ev) {
+  ev.preventDefault();
+}
+
+formElement.addEventListener("submit", handleForm);
+
+// Crear localstorage
+function setInLocalStorage() {
+  const stringFavorites = JSON.stringify(favorites);
+
+  localStorage.setItem("favorite", stringFavorites);
+}
+
+// Cargar localstorage
+function getFromLocalStorage() {
+  let localStorageFavourites = localStorage.getItem("favorite");
+
+  if (localStorageFavourites === null) {
+    favorites = [];
+  } else {
+    const arrayFav = JSON.parse(localStorageFavourites);
+    favorites = arrayFav;
+    renderFavorites();
+  }
+}
+
+getFromLocalStorage();
+
+// escuchamos elemento que clicamos
+function listenClickFavs() {
+  const liElements = document.querySelectorAll(".js-deleteIcon");
+
+  for (const liElement of liElements) {
+    liElement.addEventListener("click", deleteFav);
+  }
+}
+
+//eliminamos alguna serie que teníamos en favoritos
+function deleteFav(event) {
+  const clickDelFav = event.currentTarget;
+  const deleteFav = parseInt(clickDelFav.id);
+  const favFound = favorites.findIndex((fav) => fav.id === deleteFav);
+
+  favorites.splice(favFound, 1);
+  setInLocalStorage();
+  renderFavorites();
+  renderSeries();
+}
+
+//borramos todos los favoritos
+function deleteAllFav() {
+  favorites = [];
+
+  setInLocalStorage();
+  renderFavorites();
+  renderSeries();
+}
+deletefavElement.addEventListener("click", deleteAllFav);
